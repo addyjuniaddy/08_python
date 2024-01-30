f083561e9484b45fa9f74e9f50b320cc556ef41b
addyjuniaddy <addy.juniaddy@gmail.com>
2024-01-30 23:35:52 +0700

    Tes dari VS Code baru

--- latihan.py
+++ latihan.py
@@ -1,4 +1,4 @@
 print('coba menjalankan script python di command line')
 
 for i in range(1,11):
-      print('Saya sangat mahir bermain program python')
+      print('Saya sangat mahir bermain program python lho')
